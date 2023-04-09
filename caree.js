fc90283b9b5cd6b324219f10341c1f848dd479fd
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-09 14:36:55 +0200

    Small modification : alert on click

--- caree.js
+++ caree.js
@@ -1,3 +1,9 @@
+
+function alerCssProprietes()
+{
+alert(squareQueryBgColor + squareQueryTextcolor + squareQueryBgWidth + squareQueryHeight +squareQueryFlex + squareQueryFont + squareId) 
+}
+
 squareQuery = document.querySelector("#square")
 let squareQueryBgColor = " \n Background color: " + window.getComputedStyle(squareQuery).backgroundColor //Computed style values are returned as CSSSstyle declaration
 let squareQueryBgWidth = " \n Width: " + window.getComputedStyle(squareQuery).width                      //We need to use object notations to access properties
@@ -6,4 +12,4 @@ let squareQueryTextcolor =  " \n Text color: " + window.getComputedStyle(squareQ
 let squareQueryFlex =  " \n Display: " + window.getComputedStyle(squareQuery).display
 let squareQueryFont =  " \n Display: " + window.getComputedStyle(squareQuery).font
 let squareId = " \n ID :" + document.getElementById("square").outerHTML.slice(9, -14)           //To display only the id
-alert(squareQueryBgColor + squareQueryTextcolor + squareQueryBgWidth + squareQueryHeight +squareQueryFlex + squareQueryFont + squareId);
+squareQuery.addEventListener("click", alerCssProprietes)
